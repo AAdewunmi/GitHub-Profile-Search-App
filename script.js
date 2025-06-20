f961de7f53a0a44558dee025f52297a376b2e85d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-06-20 12:05:34 +0100

    Fifteenth Commit: Implement getGitHubProfile() function

--- script.js
+++ script.js
@@ -11,4 +11,24 @@ const errorMessage = (status) => {
   setTimeout(() => (messageDiv.innerHTML = ``), 5000);
 };
 
+const getGitHubProfile = async (login) => {
+  try {
+    const response = await fetch(`${apiBaseURL}/${login}`);
+    if (response.status !== 200) {
+      if (response.status === 404) {
+        errorMessage(response.status);
+      }
+      new Error(`Something went wrong! Status Code: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+
+
 
