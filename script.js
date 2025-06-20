56a9c819e759199e470abf780f99834620521a87
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-06-20 11:53:06 +0100

    Fourteenth Commit:  Implement errorMessage() function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const apiBaseURL = `https://api.github.com/users`;
+
+const errorMessage = (status) => {
+  const messageDiv = document.querySelector("#message");
+  let errmsg = ``;
+  if (status === 404) {
+    errmsg = `<div class="alert alert-danger text-center">Profile doesn't exist.</div>`;
+  }
+
+  messageDiv.innerHTML = errmsg;
+  setTimeout(() => (messageDiv.innerHTML = ``), 5000);
+};
+
+
