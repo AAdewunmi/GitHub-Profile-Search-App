ed732e20811799a86814f089887f6b3a6a07e5eb
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-06-21 12:01:05 +0100

    Seventeenth Commit: Implement renderProfile() function

--- script.js
+++ script.js
@@ -41,6 +41,54 @@ const getGitRepos = async (login) => {
   }
 };
 
+const renderProfile = (data) => {
+  let profileSnnipet = ``;
+  profileSnnipet += `
+             <div class="profile-userpic">
+               <img src="${data.avatar_url}" class="d-block">
+             </div>
+             <div class="profile-usertitle">
+`;
+
+  if (data.name !== null) {
+    profileSnnipet += `<div class="profile-usertitle-name">${data.name}</div>`;
+  }
+
+  profileSnnipet += `
+          <div class="profile-usertitle-job">
+          ${data.login}
+          </div>
+          </div>
+          <div class="portlet light bordered">
+          <!-- STAT -->
+          <div class="row list-separated profile-stat">
+          <div class="col-md-6 col-sm-6 col-xs-6" >
+               <div class="uppercase profile-stat-title">${data.followers}</div>
+               <div class="uppercase profile-stat-text">Followers</div>
+          </div>
+          <div class="col-md-6 col-sm-6 col-xs-6">
+          <div class="uppercase profile-stat-title">${data.following}</div>
+          <div class="uppercase profile-stat-text">Following</div>
+          </div>
+          </div>
+  `;
+  if (data.bio !== null) {
+    profileSnnipet += `<div><h4 class="profile-desc-title">About ${data.name}</h4>
+      <span class="profile-desc-text">${data.bio}</span></div>`;
+  }
+
+  if (data.twitter_username !== null) {
+    profileSnnipet += `<div class="margin-top-20 profile-desc-link">
+      <i class="fab fa-twitter"></i>
+      <a target="_blank" href="https://www.twitter.com/${data.twitter_username}">@${data.twitter_username}</a>
+      </div>`;
+  }
+
+  profileSnnipet += `</div>`;
+  document.querySelector("#profile").innerHTML = profileSnnipet;
+};
+
+
 
 
 
